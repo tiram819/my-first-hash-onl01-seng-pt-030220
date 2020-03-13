47903a85e74602d581cd1ed4c06e0abdce117db8
Tabassum Hassan <tiram819@gmail.com>
2020-03-13 06:29:55 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -43,12 +43,12 @@ def adding
   
 end
   # add 2 muskets to the shipping_manifest hash below
-  shipping_manifest["muskets"]
-  shipping_manifest["gun powder"] = 4
+  shipping_manifest["muskets"] = 2
 
 
 
   # add 4 gun powder to the shipping_manifest hash below
+  shipping_manifest["gun powder"] = 4
 
 
 
